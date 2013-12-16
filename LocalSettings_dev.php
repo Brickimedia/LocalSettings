0b5bd1d935b383da8530a65fd982c0bb4676878c
George Barnick <george.barnick@brickcraft.me>
2013-12-15 19:42:57 -0500

    moved to global

--- LocalSettings_dev.php
+++ LocalSettings_dev.php
@@ -63,6 +63,3 @@ $wgShowSQLErrors = true;
 $wgDebugLogFile = '/var/log/mw.log';
 $wgGroupPermissions['bureaucrat']['userrights'] = true;
 $wgGroupPermissions['bureaucrat']['editinterface'] = true;
-
-require_once( "$IP/extensions/WikiLove/WikiLove.php" );
-$wgDefaultUserOptions['wikilove-enabled'] = 1;

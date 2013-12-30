6b42d09bf929c384a5a1554ae7016b1f5ebaf674
George Barnick <george.barnick@brickcraft.me>
2013-12-30 13:52:08 -0500

    rv

--- LocalSettings_meta.php
+++ LocalSettings_meta.php
@@ -36,9 +36,6 @@ $wgLocalDatabases = array( 'en', 'customs', 'dev', 'stories', 'cuusoo', 'admin' 
 $wgExtraNamespaces[104] = "Board";
 $wgNamespaceProtection[104] = array( 'boardedit' ); //permission "editfoo" required to edit the foo namespace
 $wgNamespacesWithSubpages[104] = true;            //subpages enabled for the foo namespace
-$wgExtraNamespaces[105] = 'Board_talk';
-$wgNamespaceProtection[105] = array( 'boardedit');
-$wgNamespacesWithSubpages[105] = true;
 
 //uploading
 $wgEnableUploads = true;

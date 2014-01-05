d8e40b7f17525113a06c0274f21e01f126dcf902
UltrasonicNXT <adamr_carter@btinternet.com>
2014-01-05 16:44:55 +0000

    Set  = true

--- LocalSettings_ext.php
+++ LocalSettings_ext.php
@@ -170,6 +170,7 @@ require_once( "$IP/extensions/AntiSpoof/AntiSpoof.php" );
 	$gcwikis = array( 'meta', 'en', 'customs', 'cuusoo', 'stories', 'dev' );
 require_once( "$IP/extensions/GlobalContribs/GlobalContribs.php" );
 require_once( "$IP/extensions/MediaWikiChat/MediaWikiChat.php" );
+	$wgChatKicks = true;
 	$gaAccount = "UA-38958899-1";
 require_once( "$IP/extensions/googleAnalytics/googleAnalytics.php" );
 	$wgGlobalCssJsUrl = 'http://meta.brickimedia.org/index.php';

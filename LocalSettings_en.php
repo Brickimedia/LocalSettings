ad76a391dce24ce5bcc3c995a0c31ea987732035
georgebarnick <george.barnick@brickcraft.me>
2014-01-24 13:10:03 -0500

    update

--- LocalSettings_en.php
+++ LocalSettings_en.php
@@ -21,7 +21,7 @@ $wgGroupPermissions['*']['createtalk'] = true;
 $wgGroupPermissions['*']['createpage'] = true;
 
 // ArticleRatings
-require_once("$IP/extensions/are/ArticleRatings.php");
+require_once("$IP/extensions/ARE/ArticleRatings.php");
 $defaultRatingsJSONPath = "ratings_en_0.json";
 $ratingsJSONPaths = array(
 	0 => 'ratings_en_0.json'

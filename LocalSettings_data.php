957ae22222467644057af249fef972786e8f1240
George Barnick <george.barnick@brickcraft.me>
2014-03-08 14:58:40 -0500

    Update LocalSettings_data.php

--- LocalSettings_data.php
+++ LocalSettings_data.php
@@ -7,3 +7,25 @@ $wgPasswordSenderName = "Brickidata";
 
 # Wikibase
 require_once( "$IP/extensions/Wikibase/repo/Wikibase.php" );
+
+$baseNs = 100;
+ 
+// Define the namespace indexes
+define( 'WB_NS_PROPERTY', $baseNs + 2 );
+define( 'WB_NS_PROPERTY_TALK', $baseNs + 3 );
+ 
+// Define the namespaces
+$wgExtraNamespaces[WB_NS_PROPERTY] = 'Property';
+$wgExtraNamespaces[WB_NS_PROPERTY_TALK] = 'Property_talk';
+ 
+// Assigning the correct content models to the namespaces
+$wgWBRepoSettings['entityNamespaces'][CONTENT_MODEL_WIKIBASE_ITEM] = NS_MAIN; 
+$wgWBRepoSettings['entityNamespaces'][CONTENT_MODEL_WIKIBASE_PROPERTY] = WB_NS_PROPERTY;
+
+define( 'WB_NS_QUERY', $baseNs + 4 );
+define( 'WB_NS_QUERY_TALK', $baseNs + 5 );
+ 
+$wgExtraNamespaces[WB_NS_QUERY] = 'Query';
+$wgExtraNamespaces[WB_NS_QUERY_TALK] = 'Query_talk';
+ 
+$wgWBRepoSettings['entityNamespaces'][CONTENT_MODEL_WIKIBASE_QUERY] = WB_NS_QUERY;

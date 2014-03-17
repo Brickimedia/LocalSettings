9056682db0eb171eecb761133ff6450f1af84c81
George Barnick <george.barnick@brickcraft.me>
2014-03-16 20:33:40 -0400

    Update LocalSettings_data.php

--- LocalSettings_data.php
+++ LocalSettings_data.php
@@ -23,11 +23,6 @@ $wgExtraNamespaces[WB_NS_PROPERTY_TALK] = 'Property_talk';
 // Assigning the correct content models to the namespaces
 $wgWBRepoSettings['entityNamespaces'][CONTENT_MODEL_WIKIBASE_ITEM] = NS_MAIN; 
 $wgWBRepoSettings['entityNamespaces'][CONTENT_MODEL_WIKIBASE_PROPERTY] = WB_NS_PROPERTY;
-define( 'WB_NS_QUERY', $baseNs + 4 );
-define( 'WB_NS_QUERY_TALK', $baseNs + 5 );
-$wgExtraNamespaces[WB_NS_QUERY] = 'Query';
-$wgExtraNamespaces[WB_NS_QUERY_TALK] = 'Query_talk';
-$wgWBRepoSettings['entityNamespaces'][CONTENT_MODEL_WIKIBASE_QUERY] = WB_NS_QUERY;
 
 # SocialProfile
 require_once("$IP/extensions/SocialProfile/SocialProfile.php");

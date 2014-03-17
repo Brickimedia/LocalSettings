9c5601d8efccb8137af8f5d0633c8f6e60322d5b
George Barnick <george.barnick@brickcraft.me>
2014-03-17 15:41:21 -0400

    Create LocalSettings_nl.php

---
+++ LocalSettings_nl.php
@@ -0,0 +1,9 @@
+<?php
+
+$wgSitename = "Brickipedia";
+$wgMetaNamespace = "Brickipedia";
+$wgNamespaceAliases['BP'] = NS_PROJECT;
+$wgPasswordSender = "brickipedia";
+$wgPasswordSenderName = "Brickipedia";
+$wgLanguageCode = "nl";
+$wgDefaultSkin = "refreshed";

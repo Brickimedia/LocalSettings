25a5e2b66532a5ff67853add28f3549a2ab04b6a
George Barnick <george.barnick@brickcraft.me>
2014-05-28 20:02:46 -0400

    add enbricki to lmowiki refreshedheader

--- LocalSettings_lmo.php
+++ LocalSettings_lmo.php
@@ -44,6 +44,7 @@ $wgRefreshedHeader = array(
 	'img' => $refreshedLmo,
 	'dropdown' => array(
 		'http://meta.brickimedia.org/wiki/Main_Page' => $refreshedMeta,
+		'http://en.brickimedia.org/wiki/Main_Page' => $refreshedEn,
 		'http://customs.brickimedia.org/wiki/Main_Page' => $refreshedCustoms,
 		'http://stories.brickimedia.org/wiki/Main_Page' => $refreshedStories,
 		'http://ideas.brickimedia.org/wiki/Main_Page' => $refreshedIdeas,

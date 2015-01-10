042be3840676bb01c9dbdf2eaf1f8e724fb9e1db
UltrasonicNXT <adamr_carter@btinternet.com>
2015-01-10 12:30:46 +0000

    comment broken code

--- LocalSettings_dev.php
+++ LocalSettings_dev.php
@@ -89,7 +89,7 @@ $wgRefreshedHeader = array(
 		'http://stories.brickimedia.org/wiki/Main_Page' => $refreshedStories,
 		'http://ideas.brickimedia.org/wiki/Main_Page' => $refreshedIdeas,
 		//'http://books.brickimedia.org/wiki/Main_Page' => $refreshedBooks,
-		'http://minifigures.brickimedia.org/wiki/Main_Page' => $refreshedMinifigures,
+		//'http://minifigures.brickimedia.org/wiki/Main_Page' => $refreshedMinifigures,
 	)
 );
 

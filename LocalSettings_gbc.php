32fcb0b4765b302498391860bc294bb01f97dfb6
UltrasonicNXT <adamr_carter@btinternet.com>
2015-01-22 18:55:03 +0000

    Add LocalSettings_gbc.php

---
+++ LocalSettings_gbc.php
@@ -0,0 +1,11 @@
+<?php
+
+$wgSitename = "Great Ball Contraption Wiki";
+$wgMetaNamespace = "GBC";
+$wgPasswordSender = "gbc";
+$wgPasswordSenderName = "Great Ball Contraption Wiki";
+
+//$wgLogo = "http://images.brickimedia.org/d/df/BrickiCustomsLogo.png/160px-BrickiCustomsLogo.png";
+$wgFavicon = "http://images.brickimedia.org/favicons/favicon.ico";
+
+$wgLanguageCode = "en";

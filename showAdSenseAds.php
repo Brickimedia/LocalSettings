5e6c8d01f933466d8db20a700a65ddac811cee6f
George Barnick <georgebarnick@gmail.com>
2016-03-08 02:39:17 -0500

    Create showAdSenseAds.php

---
+++ showAdSenseAds.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="120">
+<style type="text/css">
+html, body {
+        margin: 0;
+        padding: 0;
+}
+</style>
+</head>
+<body>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<?php
+switch ( $_GET['size'] ) {
+        case "250":
+                echo '<!-- Square 250 -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:250px;height:250px"
+                     data-ad-client="ca-pub-9393485036030091"
+                     data-ad-slot="2497640960"></ins>';
+                break;
+        case "728":
+                echo '<!-- Leaderboard Ad -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:728px;height:90px"
+                     data-ad-client="ca-pub-9393485036030091"
+                     data-ad-slot="9488014163"></ins>';
+                break;
+        case "320":
+                echo '<!-- Mobile 320 Ad -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:320px;height:100px"
+                     data-ad-client="ca-pub-9393485036030091"
+                     data-ad-slot="4824678565"></ins>';
+                break;
+        default:
+                echo '<ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-9393485036030091"
+                     data-ad-slot="7756942160"
+                     data-ad-format="auto"></ins>';
+                break;
+}
+?>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</body>
+</html>

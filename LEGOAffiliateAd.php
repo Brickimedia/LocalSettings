825c73d2f13722777b4964c6bf1f387c035fdc99
George Barnick <georgebarnick@gmail.com>
2016-03-08 17:06:52 -0500

    Create LEGOAffiliateAd.php

---
+++ LEGOAffiliateAd.php
@@ -0,0 +1,190 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="120">
+<style type="text/css">
+html, body {
+        margin: 0;
+        padding: 0;
+}
+</style>
+</head>
+<body>
+<?php
+switch ( $_GET['theme'] ) {
+  case "starwars":
+    switch ( $_GET['size'] ) {
+      case "125":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001180&type=4&subid=0"><IMG alt="Travel the dunes with the LEGO� Star Wars™ Ultimate Collector Series Sancrawler™" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2014/sandcrawler/star-wars-75059-125x125.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001180&type=4&subid=0">';
+        break;
+      case "728":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001184&type=4&subid=0"><IMG alt="Travel the dunes with the LEGO� Star Wars™ Ultimate Collector Series Sancrawler™" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2014/sandcrawler/star-wars-75059-728x90.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001184&type=4&subid=0">';
+        break;
+      case "120":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001179&type=4&subid=0"><IMG alt="Travel the dunes with the LEGO� Star Wars™ Ultimate Collector Series Sancrawler™" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2014/sandcrawler/star-wars-75059-120x90.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001179&type=4&subid=0">';
+        break;
+      case "160":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001185&type=4&subid=0"><IMG alt="Travel the dunes with the LEGO� Star Wars™ Ultimate Collector Series Sancrawler™" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2014/sandcrawler/star-wars-75059-160x600.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001185&type=4&subid=0">';
+        break;
+      case "234":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001181&type=4&subid=0"><IMG alt="Travel the dunes with the LEGO� Star Wars™ Ultimate Collector Series Sancrawler™" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2014/sandcrawler/star-wars-75059-234x60.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001181&type=4&subid=0">';
+        break;
+      default:
+        echo 'Error: Required paramter "size" not specified.';
+        break;
+    }
+    break;
+  case "duplo":
+    switch ( $_GET['size'] ) {
+      case "125":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001380&type=4&subid=0"><IMG alt="FREE LEGO® DUPLO® Duck with any LEGO DUPLO purchase!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/30321-duck/30321-duck-125x125.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001380&type=4&subid=0">';
+        break;
+      case "728":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001383&type=4&subid=0"><IMG alt="FREE LEGO® DUPLO® Duck with any LEGO DUPLO purchase!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/30321-duck/30321-duck-728x90.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001383&type=4&subid=0">';
+        break;
+      case "120":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001379&type=4&subid=0"><IMG alt="FREE LEGO® DUPLO® Duck with any LEGO DUPLO purchase!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/30321-duck/30321-duck-120x90.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001379&type=4&subid=0">';
+        break;
+      case "160":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001385&type=4&subid=0"><IMG alt="FREE LEGO® DUPLO® Duck with any LEGO DUPLO purchase!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/30321-duck/30321-duck-160x600.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001385&type=4&subid=0">';
+        break;
+      case "234":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001381&type=4&subid=0"><IMG alt="FREE LEGO® DUPLO® Duck with any LEGO DUPLO purchase!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/30321-duck/30321-duck-234x60.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001381&type=4&subid=0">';
+        break;
+      default:
+        echo 'Error: Required paramter "size" not specified.';
+        break;
+    }
+    break;
+  case "ninjago":
+    switch ( $_GET['size'] ) {
+      case "125":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001388&type=4&subid=0"><IMG alt="FREE Kai's Mini Dragon with purchases of $35 or more!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/30422-dragon/30422-dragon-125x125.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001388&type=4&subid=0">';
+        break;
+      case "728":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001391&type=4&subid=0"><IMG alt="FREE Kai's Mini Dragon with purchases of $35 or more!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/30422-dragon/30422-dragon-728x90.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001391&type=4&subid=0">';
+        break;
+      case "120":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001387&type=4&subid=0"><IMG alt="FREE Kai's Mini Dragon with purchases of $35 or more!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/30422-dragon/30422-dragon-120x90.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001387&type=4&subid=0">';
+        break;
+      case "160":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001393&type=4&subid=0"><IMG alt="FREE Kai's Mini Dragon with purchases of $35 or more!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/30422-dragon/30422-dragon-160x600.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001393&type=4&subid=0">';
+        break;
+      case "234":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001389&type=4&subid=0"><IMG alt="FREE Kai's Mini Dragon with purchases of $35 or more!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/30422-dragon/30422-dragon-234x60.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001389&type=4&subid=0">';
+        break;
+      default:
+        echo 'Error: Required paramter "size" not specified.';
+        break;
+    }
+    break;
+  case "city":
+    switch ( $_GET['size'] ) {
+      case "125":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001356&type=4&subid=0"><IMG alt="New LEGO® City Prison Island sets available now!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2015/city-2016/prison-island/city-2016-prison-island-125x125.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001356&type=4&subid=0">';
+        break;
+      case "728":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001359&type=4&subid=0"><IMG alt="New LEGO® City Prison Island sets available now!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2015/city-2016/prison-island/city-2016-prison-island-728x90.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001359&type=4&subid=0">';
+        break;
+      case "120":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001347&type=4&subid=0"><IMG alt="New LEGO® City Fire sets available now!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2015/city-2016/fire/city-2016-fire-120x90.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001347&type=4&subid=0">';
+        break;
+      case "160":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001353&type=4&subid=0"><IMG alt="New LEGO® City Fire sets available now!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2015/city-2016/fire/city-2016-fire-160x600.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001353&type=4&subid=0">';
+        break;
+      case "234":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001349&type=4&subid=0"><IMG alt="New LEGO® City Fire sets available now!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2015/city-2016/fire/city-2016-fire-234x60.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001349&type=4&subid=0">';
+        break;
+      default:
+        echo 'Error: Required paramter "size" not specified.';
+        break;
+    }
+    break;
+  case "friends":
+    switch ( $_GET['size'] ) {
+      case "125":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001340&type=4&subid=0"><IMG alt="Explore Heartlake City with brand new LEGO® Friends sets!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2015/friends-2016/friends-2016-125x125.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001340&type=4&subid=0">';
+        break;
+      case "728":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001343&type=4&subid=0"><IMG alt="Explore Heartlake City with brand new LEGO® Friends sets!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2015/friends-2016/friends-2016-728x90.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001343&type=4&subid=0">';
+        break;
+      case "120":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001339&type=4&subid=0"><IMG alt="Explore Heartlake City with brand new LEGO® Friends sets!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2015/friends-2016/friends-2016-120x90.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001339&type=4&subid=0">';
+        break;
+      case "160":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001345&type=4&subid=0"><IMG alt="Explore Heartlake City with brand new LEGO® Friends sets!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2015/friends-2016/friends-2016-160x600.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001345&type=4&subid=0">';
+        break;
+      case "234":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001341&type=4&subid=0"><IMG alt="Explore Heartlake City with brand new LEGO® Friends sets!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2015/friends-2016/friends-2016-234x60.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001341&type=4&subid=0">';
+        break;
+      default:
+        echo 'Error: Required paramter "size" not specified.';
+        break;
+    }
+    break;
+  case "creator":
+    switch ( $_GET['size'] ) {
+      case "125":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001229&type=4&subid=0"><IMG alt="Take the iconic LEGO MINI Cooper for a drive!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2014/Cooper/cooper-10242-125x125-en.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001229&type=4&subid=0">';
+        break;
+      case "728":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001231&type=4&subid=0"><IMG alt="Take the iconic LEGO MINI Cooper for a drive!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2014/Cooper/cooper-10242-728x90-en.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001231&type=4&subid=0">';
+        break;
+      case "120":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001227&type=4&subid=0"><IMG alt="Take the iconic LEGO MINI Cooper for a drive!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2014/Cooper/cooper-10242-120x90-en.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001227&type=4&subid=0">';
+        break;
+      case "160":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001233&type=4&subid=0"><IMG alt="Take the iconic LEGO MINI Cooper for a drive!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2014/Cooper/cooper-10242-160x600-en.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001233&type=4&subid=0">';
+        break;
+      case "234":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001228&type=4&subid=0"><IMG alt="Take the iconic LEGO MINI Cooper for a drive!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2014/Cooper/cooper-10242-234x60-en.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001228&type=4&subid=0">';
+        break;
+      default:
+        echo 'Error: Required paramter "size" not specified.';
+        break;
+    }
+    break;
+  case "easter":
+    switch ( $_GET['size'] ) {
+      case "125":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001372&type=4&subid=0"><IMG alt="The LEGO® Easter Sale is on! Save up to 20% on select items!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/easter-sale/easter-sale-125x125.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001372&type=4&subid=0">';
+        break;
+      case "728":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001375&type=4&subid=0"><IMG alt="The LEGO® Easter Sale is on! Save up to 20% on select items!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/easter-sale/easter-sale-728x90.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001375&type=4&subid=0">';
+        break;
+      case "120":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001371&type=4&subid=0"><IMG alt="The LEGO® Easter Sale is on! Save up to 20% on select items!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/easter-sale/easter-sale-120x90.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001371&type=4&subid=0">';
+        break;
+      case "160":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001377&type=4&subid=0"><IMG alt="The LEGO® Easter Sale is on! Save up to 20% on select items!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/easter-sale/easter-sale-160x600.jpg"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001377&type=4&subid=0">';
+        break;
+      case "234":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10001373&type=4&subid=0"><IMG alt="The LEGO® Easter Sale is on! Save up to 20% on select items!" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/2016/easter-sale/easter-sale-234x60.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10001373&type=4&subid=0">';
+        break;
+      default:
+        echo 'Error: Required paramter "size" not specified.';
+        break;
+    }
+    break;
+  default:
+    switch ( $_GET['size'] ) {
+      case "125":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=96622.10000016&type=4&subid=0"><IMG  width="125" height="125" alt="Lego_SalesDeals_125x125" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/LEGO_FreeShipping_125x125.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=96622.10000016&type=4&subid=0">';
+        break;
+      case "728":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10000434&type=4&subid=0"><IMG alt="LEGO Brand Retail" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/LEGOgeneric_728x90.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10000434&type=4&subid=0">';
+        break;
+      case "120":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10000103&type=4&subid=0"><IMG alt="LEGO" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/LEGO_logo_120x60.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10000103&type=4&subid=0">';
+        break;
+      case "160":
+        echo '<a href="http://click.linksynergy.com/fs-bin/click?id=flo3EmTGydA&offerid=115554.10000435&type=4&subid=0"><IMG alt="LEGO Brand Retail" border="0" src="http://cache.lego.com/2057/images/shop/Affiliate/LEGOgeneric_160x600.gif"></a><IMG border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=flo3EmTGydA&bids=115554.10000435&type=4&subid=0">';
+        break;
+      default:
+        echo 'Error: Required paramter "size" not specified.';
+        break;
+    }
+    break;
+}
+?>
+</body>
+</html>

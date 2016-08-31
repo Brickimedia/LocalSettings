5a84fb2875a7e30b270737cf6892680fd5148252
Samantha Nguyen <SamanthaNguyen@users.noreply.github.com>
2016-08-31 18:43:53 -0500

    tabbing consistency, declaring character encoding type

--- showAdSenseAds.php
+++ showAdSenseAds.php
@@ -1,49 +1,50 @@
 <html>
-<head>
-<meta http-equiv="refresh" content="120">
-<style type="text/css">
-html, body {
-        margin: 0;
-        padding: 0;
-}
-</style>
-</head>
-<body>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<?php
-switch ( $_GET['size'] ) {
-        case "250":
-                echo '<!-- Square 250 -->
-                <ins class="adsbygoogle"
-                     style="display:inline-block;width:250px;height:250px"
-                     data-ad-client="ca-pub-9543775174763951"
-                     data-ad-slot="8311981130"></ins>';
-                break;
-        case "728":
-                echo '<!-- Leaderboard Ad -->
-                <ins class="adsbygoogle"
-                     style="display:inline-block;width:728px;height:90px"
-                     data-ad-client="ca-pub-9543775174763951"
-                     data-ad-slot="5358514734"></ins>';
-                break;
-        case "320":
-                echo '<!-- Mobile 320 Ad -->
-                <ins class="adsbygoogle"
-                     style="display:inline-block;width:320px;height:100px"
-                     data-ad-client="ca-pub-9543775174763951"
-                     data-ad-slot="6835247939"></ins>';
-                break;
-        default:
-                echo '<ins class="adsbygoogle"
-                    style="display:block"
-                    data-ad-client="ca-pub-9543775174763951"
-                    data-ad-slot="9788714334"
-                    data-ad-format="auto"></ins>';
-                break;
-}
-?>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</body>
+        <head>
+                <meta http-equiv="refresh" content="120">
+                <meta charset="UTF-8">
+                <style type="text/css">
+                        html, body {
+                                margin: 0;
+                                padding: 0;
+                        }
+                </style>
+        </head>
+        <body>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <?php
+        switch ( $_GET['size'] ) {
+                case "250":
+                        echo '<!-- Square 250 -->
+                        <ins class="adsbygoogle"
+                             style="display:inline-block;width:250px;height:250px"
+                             data-ad-client="ca-pub-9543775174763951"
+                             data-ad-slot="8311981130"></ins>';
+                        break;
+                case "728":
+                        echo '<!-- Leaderboard Ad -->
+                        <ins class="adsbygoogle"
+                             style="display:inline-block;width:728px;height:90px"
+                             data-ad-client="ca-pub-9543775174763951"
+                             data-ad-slot="5358514734"></ins>';
+                        break;
+                case "320":
+                        echo '<!-- Mobile 320 Ad -->
+                        <ins class="adsbygoogle"
+                             style="display:inline-block;width:320px;height:100px"
+                             data-ad-client="ca-pub-9543775174763951"
+                             data-ad-slot="6835247939"></ins>';
+                        break;
+                default:
+                        echo '<ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-9543775174763951"
+                            data-ad-slot="9788714334"
+                            data-ad-format="auto"></ins>';
+                        break;
+        }
+        ?>
+        <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+        </body>
 </html>

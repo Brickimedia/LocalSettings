f71c8f1be0322b0c8ab4dcbf7ffdefa7c4e6572f
Samantha Nguyen <SamanthaNguyen@users.noreply.github.com>
2016-09-13 19:13:57 -0500

    code nitpicking, removing questions for CAPTCHA as they were moved to LocalSettings_private.php
    https://phabricator.wikimedia.org/T142629

--- LocalSettings_ext.php
+++ LocalSettings_ext.php
@@ -35,11 +35,11 @@ wfLoadExtensions( array(
 ) );
 
 // Not yet on new loading system:
-require_once "$IP/extensions/AbuseFilter/AbuseFilter.php";
-require_once "$IP/extensions/GlobalPreferences/GlobalPreferences.php";
-require_once "$IP/extensions/WikiLove/WikiLove.php";
-require_once "$IP/extensions/Thanks/Thanks.php";
-require_once "$IP/extensions/QuizGame/QuestionGame.php";
+require_once '$IP/extensions/AbuseFilter/AbuseFilter.php';
+require_once '$IP/extensions/GlobalPreferences/GlobalPreferences.php';
+require_once '$IP/extensions/WikiLove/WikiLove.php';
+require_once '$IP/extensions/Thanks/Thanks.php';
+require_once '$IP/extensions/QuizGame/QuestionGame.php';
 
 // Cite extension settings
 $wgCiteEnablePopups = true; // Pop-up citations
@@ -50,30 +50,30 @@ $wgGroupPermissions['sysop']['commentadmin'] = true;
 $wgCommentsInRecentChanges = true;
 $wgCommentsSortDescending = true;
 
-require_once "$IP/extensions/CSS/CSS.php";
-require_once "$IP/extensions/DPLForum/DPLforum.php";
-require_once "$IP/extensions/GlobalBlocking/GlobalBlocking.php";
+require_once '$IP/extensions/CSS/CSS.php';
+require_once '$IP/extensions/DPLForum/DPLforum.php';
+require_once '$IP/extensions/GlobalBlocking/GlobalBlocking.php';
 
 // GlobalPreferences settings
 $wgGlobalPreferencesDB = 'shared';
 
-require_once "$IP/extensions/MediawikiPlayer/MediawikiPlayer.php";
+require_once '$IP/extensions/MediawikiPlayer/MediawikiPlayer.php';
 // ParserFunctions settings
 	$wgAllowSlowParserFunctions = true;
 	$wgPFStringLengthLimit = 10000;
 	$wgPFEnableStringFunctions = true;
-require_once "$IP/extensions/Quantcast/Quantcast.php";
-require_once "$IP/extensions/RandomSelection/RandomSelection.php";
-require_once "$IP/extensions/UserMerge/UserMerge.php";
-require_once "$IP/extensions/VideoFlash/VideoFlash.php"; // both VideoFlash and Embed video for the mo
+require_once '$IP/extensions/Quantcast/Quantcast.php';
+require_once '$IP/extensions/RandomSelection/RandomSelection.php';
+require_once '$IP/extensions/UserMerge/UserMerge.php';
+require_once '$IP/extensions/VideoFlash/VideoFlash.php'; // both VideoFlash and Embed video for the mo
 // WikiLove settings
 	$wgDefaultUserOptions['wikilove-enabled'] = 1;
-require_once "$IP/extensions/PageInCat/PageInCat.php";
-require_once "$IP/extensions/NumerAlpha/NumerAlpha.php";
+require_once '$IP/extensions/PageInCat/PageInCat.php';
+require_once '$IP/extensions/NumerAlpha/NumerAlpha.php';
 
 // SocialProfile. More details in individual LSs
-require_once "$IP/extensions/SocialProfile/SocialProfile.php";
-require_once "$IP/extensions/SocialProfile/UserStats/EditCount.php"; // Necessary edit counter
+require_once '$IP/extensions/SocialProfile/SocialProfile.php';
+require_once '$IP/extensions/SocialProfile/UserStats/EditCount.php'; // Necessary edit counter
 unset( $wgSpecialPages['GiveGift'] ); // remove Special:GiveGift
 unset( $wgSpecialPages['ViewGifts'] ); // remove Special:ViewGifts
 $wgAvatarKey = 'global'; // global avatars
@@ -83,24 +83,17 @@ $wgUserProfileThresholds = array( 'edits' => 5 ); // preventing spam
 $wgEnableUploads = true;
 
 // Stop those spammers!
-require_once "$IP/extensions/ConfirmEdit/ConfirmEdit.php";
-require_once "$IP/extensions/ConfirmEdit/QuestyCaptcha.php";
+require_once '$IP/extensions/ConfirmEdit/ConfirmEdit.php';
+require_once '$IP/extensions/ConfirmEdit/QuestyCaptcha.php';
 $wgCaptchaClass = 'QuestyCaptcha';
-$arr = array(
-	'What is Brickimedia about?' => 'LEGO',
-	'Please write the magic word, "energy", here:' => 'energy',
-	'Type the code word, 329, here:' => '329',
-	'What is half of 6?' => '3',
-	'Write the number 4 in letters:' => 'four',
-);
 foreach ( $arr as $key => $value ) {
 	$wgCaptchaQuestions[] = array( 'question' => $key, 'answer' => $value );
 }
 
 // QuestyCaptcha is unreliable. --Seahorse
-// require_once "$IP/extensions/ConfirmEdit/FancyCaptcha.php";
+// require_once '$IP/extensions/ConfirmEdit/FancyCaptcha.php';
 // $wgCaptchaClass = 'FancyCaptcha';
-// $wgCaptchaDirectory = "/var/www/images/captcha";
+// $wgCaptchaDirectory = '/var/www/images/captcha';
 // $wgCaptchaDirectoryLevels = 0; // Set this to a value greater than zero to break the images into subdirectories
 $wgGroupPermissions['autoconfirmed']['skipcaptcha'] = true;
 $wgGroupPermissions['bot']['skipcaptcha'] = true;
@@ -109,7 +102,7 @@ $wgGroupPermissions['sysop']['skipcaptcha'] = true;
 // $wgCaptchaTriggers['edit'] = true;
 
 // Short URLs
-$wgArticlePath = "/wiki/$1";
+$wgArticlePath = '/wiki/$1';
 $wgUsePathInfo = true;
 
 // WikiEditor
@@ -171,7 +164,7 @@ $wgDefaultUserOptions['usenewrc'] = 1;
 $wgShowExceptionDetails = true;
 
 // WikiForum
-require_once "$IP/extensions/WikiForum/WikiForum.php";
+require_once '$IP/extensions/WikiForum/WikiForum.php';
 	$wgWikiForumAllowAnonymous = true; // CAPTCHAs now shown
 	unset( $wgAvailableRights['wikiforum-admin'] );
 	unset( $wgAvailableRights['wikiforum-moderator'] );
@@ -186,7 +179,7 @@ $wgCopyUploadsFromSpecialUpload = true;
 $wgAllowExternalImages = true;
 
 // HTML (feel free to disable; testing still)
-include_once "$IP/extensions/HTMLTags/HTMLTags.php";
+include_once '$IP/extensions/HTMLTags/HTMLTags.php';
 	$wgHTMLTagsAttributes['iframe'] = array( 'src', 'width', 'height', 'style', 'scrolling' );
 	$wgHTMLTagsAttributes['form'] = array( 'action', 'method' );
 	$wgHTMLTagsAttributes['input'] = array( 'type', 'name', 'value', 'src', 'border', 'alt' );
@@ -206,15 +199,15 @@ $wgPdfInfo = 'pdfinfo';
 // GlobalUsage settings
 $wgGlobalUsageDatabase = 'meta';
 
-require_once "$IP/extensions/AntiSpoof/AntiSpoof.php";
-require_once "$IP/extensions/GlobalContribs/GlobalContribs.php";
-require_once "$IP/extensions/MediaWikiChat/MediaWikiChat.php";
+require_once '$IP/extensions/AntiSpoof/AntiSpoof.php';
+require_once '$IP/extensions/GlobalContribs/GlobalContribs.php';
+require_once '$IP/extensions/MediaWikiChat/MediaWikiChat.php';
 	$wgChatKicks = true;
 	$wgChatMeCommand = true;
 	$wgChatLinkUsernames = true;
 
 // Global new talk page message alerts
-require_once "$IP/extensions/NewTalkGlobal/NewTalkGlobal.php";
+require_once '$IP/extensions/NewTalkGlobal/NewTalkGlobal.php';
 $newTalkGlobalDatabases = array(
 	'meta' => array(
 		'db' => 'meta',
@@ -255,7 +248,7 @@ $wgLocalDatabases = array( 'en', 'customs', 'gbc', 'ideas', 'meta');
 $wgConf->wikis = $wgLocalDatabases;
 
 // Echo
-require_once "$IP/extensions/Echo/Echo.php";
+require_once '$IP/extensions/Echo/Echo.php';
 
 // Thanks settings
 $wgThanksSendToBots = false;
@@ -263,10 +256,10 @@ $wgThanksLogging = true;
 $wgThanksConfirmationRequired = true;
 
 // User rights
-require_once "$IP/UserRights.php";
+require_once '$IP/UserRights.php';
 
 // More extensions
-require_once "$IP/extensions/Renameuser/Renameuser.php";
+require_once '$IP/extensions/Renameuser/Renameuser.php';
 
 // SpamBlacklist settings
 $wgBlacklistSettings['email']['files'] = array( 'http://meta.brickimedia.org/index.php?title=MediaWiki:Email-blacklist&action=raw&sb_ver=1' );
